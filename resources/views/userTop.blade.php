d875f5e1295b08b7cc39842f18c12209d204a9a2
ts44gj <ts44gj2020@gmail.com>
2021-11-20 09:17:40 +0900

    toppage

--- resources/views/userTop.blade.php
+++ resources/views/userTop.blade.php
@@ -10,41 +10,6 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-6">
-                    <div class="card card-stats">
-                        <div class="card-header card-header-warning card-header-icon">
-                            <div class="card-icon">
-                                <i class="material-icons">content_copy</i>
-                            </div>
-                            <p class="card-category">Used Space</p>
-                            <h3 class="card-title">49/50
-                                <small>GB</small>
-                            </h3>
-                        </div>
-                        <div class="card-footer">
-                            <div class="stats">
-                                <i class="material-icons text-danger">warning</i>
-                                <a href="javascript:;">Get More Space...</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="card card-chart">
-                        <div class="card-header card-header-danger">
-                            <div class="ct-chart" id="completedTasksChart"></div>
-                        </div>
-                        <div class="card-body">
-                            <h4 class="card-title">Completed Tasks</h4>
-                            <p class="card-category">Last Campaign Performance</p>
-                        </div>
-                        <div class="card-footer">
-                            <div class="stats">
-                                <i class="material-icons">access_time</i> campaign sent 2 days ago
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
                     <div class="card">
                         <div class="card-header card-header-tabs card-header-primary">
                             <div class="nav-tabs-navigation">
