d2af1a444e490516f28ca2eb6c369867d5ab012c
ts44gj <ts44gj2020@gmail.com>
2021-12-04 00:55:16 +0900

    s

--- resources/views/memos/index.blade.php
+++ resources/views/memos/index.blade.php
@@ -118,31 +118,7 @@
     </div>
     <!--</table>-->
     {{ $memos->links() }}
-    </div>
-    <!-- モーダル
-    <div id="modal-delete-{{ $memo->id }}" class="modal fade" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <form method="POST" action="{{ route('memos.destroy', ['memo' => $memo]) }}">
-                    @csrf
-                    @method('DELETE')
-                    <div class="modal-body">
-                        {{ $memo->title }}を削除します。よろしいですか？
-                    </div>
-                    <div class="modal-footer justify-content-between">
-                        <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
-                        <button type="submit" class="btn btn-danger">削除する</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    モーダル -->
+  
     </div>
     </div>
 @endsection
